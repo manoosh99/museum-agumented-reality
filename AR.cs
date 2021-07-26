7d61335d87d3dcc2dcfd23741d14950a81b34656
manoosh99 <76001226+manoosh99@users.noreply.github.com>
2021-07-26 21:20:13 +0300

    Add files via upload

---
+++ AR.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AR : MonoBehaviour {
+
+	// Use this for initialization
+	public void SceneAR()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
